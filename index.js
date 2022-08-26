e830bcfaeaceb95579ed943a1bae2fe98e89e06c
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-08-26 23:17:53 +0000

    se modifica el nombre

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ let dominio = [".cl", ".com", ".net"];
 
 for( let i=0; i < pronombre.length; i++){
     for(let j=0; j < adjetivo.length; j++){
-        for(let p=0; p < sustantivo.length; p++){
-            for(let b=0; b < dominio.length; b++){
-                let mail = pronombre[i] + adjetivo[j] + sustantivo[p] + dominio[b];
+        for(let k=0; k < sustantivo.length; k++){
+            for(let l=0; l < dominio.length; l++){
+                let nombres = pronombre[i] + adjetivo[j] + sustantivo[k] + dominio[l];
             }
         }
     }
 }
-console.log(mail);
+
+console.log(nombres)
