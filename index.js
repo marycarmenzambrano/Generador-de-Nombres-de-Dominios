2bab6602ee712cc106cd9f6058fedf5ef231f35c
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-08-26 02:43:59 +0000

    se agrega texto

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let pronoun = ['the','our'];
-let adj = ['great', 'big' ];
-let noun = ['jogger','racoon'];
+let pronombre = ["la", "nuestra"];
+let adjetivo = ["estupenda", "grande "];
+let sustantivo = ["corredora", "mapache",];
+let dominio = [".cl", ".com", ".net"];
 
-console.log(pronoun);
+for( let i=0; i < pronombre.length; i++){
+    for(let j=0; j < adjetivo.length; j++){
+        for(let p=0; p < sustantivo.length; p++){
+            for(let b=0; b < dominio.length; b++){
+                let mail = pronombre[i] + adjetivo[j] + sustantivo[p] + dominio[b];
+            }
+        }
+    }
+}
+console.log(mail);
