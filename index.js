5214cec63f16573f9f73754bf92c493f9e5d4c55
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-08-28 17:49:11 +0000

    elimina una linea

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let pronombre = ["la", "nuestra"];
-let adjetivo = ["estupenda", "grande "];
-let sustantivo = ["corredora", "mapache",];
-let dominio = [".cl", ".com", ".net"];
+let pronombre = ["la","nuestra"];
+let adjetivo = ["estupenda","grande "];
+let sustantivo = ["corredora","mapache",];
+let dominio = [".cl",".com",".net"];
 
 for( let i=0; i < pronombre.length; i++){
     for(let j=0; j < adjetivo.length; j++){
         for(let k=0; k < sustantivo.length; k++){
             for(let l=0; l < dominio.length; l++){
-                let nombres = pronombre[i] + adjetivo[j] + sustantivo[k] + dominio[l];
+                console.log(pronombre[i] + adjetivo[j] + sustantivo[k] + dominio[l]);
             }
         }
     }
 }
-console.log(nombres)
+
